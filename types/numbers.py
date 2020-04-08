df4d3597ade62497d55c6ea39c63ec9ff0bc2640
JpabloMF <juanpablo45671@gmail.com>
2020-04-08 08:36:20 -0500

    types numbers/strings

---
+++ types/numbers.py
@@ -0,0 +1,21 @@
+# NUMBERS
+# we have two types
+# integer
+# float === decimal
+
+# INTEGER
+print(5+6)
+
+# CONCAT
+print("5"+"6")
+
+# FLOAT
+print(5.23*6.1)
+print(5.5+5.5)
+
+# CONVERT STRING TO INTEGER
+int("5")
+print(int("5"))
+
+int("5") + int("7")
+print(int("5") + int("7"))
---
+++ types/strings.py
@@ -0,0 +1,10 @@
+# SINGLE QUOTE
+print('Hello')
+print('She said "I want this"')
+
+# DOUBLE QUOTE
+print("Hello")
+print("Don't do that")
+
+# TAKE A QUOTE AS A STRING (ESCAPING)
+print('She said "Don\'t do that"')
