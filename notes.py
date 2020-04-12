63cb4daebbe71da0d1d33650571df4341ad06eae
JpabloMF <juanpablo45671@gmail.com>
2020-04-12 00:01:14 -0500

    notes, calculator project

---
+++ notes.py
@@ -0,0 +1,6 @@
+# use [global variable_name]
+# to have the scope of a variable out of our function
+
+# use [eval(string)]  - ONLY in specific cases or being evaluated by a regex - is avoided
+# to evaluate complex mathematical operations from strings
+# eval('80+2') -> 82
