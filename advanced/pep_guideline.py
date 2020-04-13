8ef511c24db1afc5abbd70f70d05fdecc0a730da
JpabloMF <juanpablo45671@gmail.com>
2020-04-12 22:20:03 -0500

    pep guideline,continue while, breaking out while loop

---
+++ advanced/pep_guideline.py
@@ -0,0 +1,23 @@
+import os
+import sys
+# from MyModule import foo, bar, foobar
+
+
+def my_function(one, two,
+                three, four,
+                five, fix):
+    print("Hello World")
+
+
+def second_function():
+    print("Second function")
+
+
+my_list = [1, 2,
+           3, 4,
+           5, 6]
+
+check = True
+
+if check is True:
+    print("This is true")
